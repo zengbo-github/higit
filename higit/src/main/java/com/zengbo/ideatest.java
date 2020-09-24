f01ba560fea2a26ece70b1c903d1eaccb37456ab
zengbo <875787599@qq.com>
2020-09-25 07:13:29 +0800

    创建分支

--- higit/src/main/java/com/zengbo/ideatest.java
+++ higit/src/main/java/com/zengbo/ideatest.java
@@ -9,5 +9,10 @@ public class ideatest {
     public static void main(String[] args) {
         System.out.println("更新1");
         System.out.println("更新2");
+        System.out.println("更新3");
+        System.out.println("更新4");
+        System.out.println("更新5");
+        System.out.println("创建分支");
+
     }
 }
